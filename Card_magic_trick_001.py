bf91c3edf76131831c24d8e03f7f08961948827d
Waii <76773473+Zinwaiyan274@users.noreply.github.com>
2021-12-20 16:34:55 +0630

    Update Card_magic_trick_001.py

--- Card_magic_trick_001.py
+++ Card_magic_trick_001.py
@@ -1,17 +1,23 @@
 import random
 
-raw_card = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "King", "Queen", "Ace"]
-card_suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 
-suits = random.choice(card_suits)
-raw = random.choice(raw_card)
+def computer_picky():
+    raw_card = ["2", "3", "4", "5", "6", "7", "8", "9", "Jack", "King", "Queen", "Ace"]
+    card_suits = [" Hearts", "Diamonds", "Spades", "Clubs"]
 
-without_s = raw_card[:1]
-removed_s = suits[:-1]
-if raw in without_s:
+    suits = random.choice(card_suits)
+    raw = random.choice(raw_card)
 
-    print(f"{raw} of {removed_s}")
+    without_s = raw_card[-1]
+    removed_s = suits[:-1]
+    if raw in without_s:
 
-else:
+        print(f"{raw} of {removed_s}")
 
-    print(f"{raw} of {suits}")
+    else:
+
+        print(f"{raw} of {suits}")
+
+
+if __name__ == '__main__':
+    computer_picky()
