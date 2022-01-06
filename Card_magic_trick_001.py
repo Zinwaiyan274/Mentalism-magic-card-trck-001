c890404dd2b32557ad634184d013c7f77d625680
Waii <76773473+Zinwaiyan274@users.noreply.github.com>
2022-01-06 08:33:48 +0630

    Update Card_magic_trick_001.py

--- Card_magic_trick_001.py
+++ Card_magic_trick_001.py
@@ -23,7 +23,6 @@ if __name__ == '__main__':
     computer_picky()
 
     
-    
     import speech_recognition as sr
 
 card = ['ace of spade','ace of heart','ace of diamond','ace of club','two of spades','two of hearts','two of diamonds','two of clubs','three of spades','three of hearts','three of diamonds','three of clubs','four of spades','four of hearts','four of diamonds','four of clubs','five of spades','five of hearts','five of diamonds','five of clubs','six of spades','six of hearts','six of diamonds','six of clubs','seven of spades','seven of hearts','seven of diamonds','seven of clubs','eight of spades','eight of hearts','eight of diamonds','eight of clubs','nine of spades','nine of hearts','nine of diamonds','nine of clubs','ten of spades','ten of hearts','ten of diamonds','ten of clubs','jack of spades','jack of hearts','jack of diamonds','jack of clubs','queen of spades','queen of hearts','queen of diamonds','queen of clubs','king of spades','king of hearts','king of diamonds','king of clubs']
@@ -40,16 +39,18 @@ with sr.Microphone() as source:
 
     except:
         print("Sorry could not recognize what you said")
-# text = str(text).lower()
+text = str(text).lower()
 
-print(text)
+# print(text)
 
 
-#
-# if text in card:
-#     print(f"volia! keyword is >>>{card}<<<< and you said >>>>{text}<<<<<")
-# else:
-#     ("Dammm")
+
+if text in card:
+    print(f"volia! keyword is >>>{card}<<<< and you said >>>>{text}<<<<<")
+else:
+    print(f"Damm you said>>{text}<<")
+
+
 
 # ___________________Not done yet_____________________
 
